2a284cfc37ff558ba53cf481106cab2c8fea214e
Ben Alcantara <ben.alcantara@gmail.com>
2017-11-15 13:46:37 +1100

    Use bootstrap-sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Heading from './components/Heading';
 import Planets from './components/Planets';
-import './App.scss';
+import './styles/App.css';
 
 class App extends Component {
   constructor(){
@@ -16,9 +16,27 @@ class App extends Component {
         },
         {
           name: 'Cronenberg World',
-          img: 'https://vignette.wikia.nocookie.net/rickandmorty/images/e/e3/No_Image.png/revision/latest/scale-to-width-down/310?cb=20160908213759',
+          img: 'https://vignette.wikia.nocookie.net/rickandmorty/images/7/7a/Screenshot_2015-10-05_at_1.12.51_PM.png/revision/latest/scale-to-width-down/310?cb=20151005171418',
           description: 'Cronenberg World is Earth in a dimension where people on Earth are naturally Cronenbergs.',
-          price: '10 schmeckles'
+          price: '2 schmeckles'
+        },
+        {
+          name: 'Planet Squanch',
+          img: 'https://vignette.wikia.nocookie.net/rickandmorty/images/e/e3/No_Image.png/revision/latest/scale-to-width-down/310?cb=20160908213759',
+          description: 'A planet where Squanchy comes from and it is 6,000 light years away from Earth.',
+          price: '17 schmeckles'
+        },
+        {
+          name: 'Flarbellon-7',
+          img: 'https://vignette.wikia.nocookie.net/rickandmorty/images/5/56/Flarbellon7House.png/revision/latest/scale-to-width-down/310?cb=20170508211706',
+          description: 'Flarbellon-7 is a completely deserted planet only inhabited by Penps, insects, and some weird shrubs.',
+          price: '12 schmeckles'
+        },
+        {
+          name: 'Dwarf Terrace-9',
+          img: 'https://vignette.wikia.nocookie.net/rickandmorty/images/c/c4/Screenshot_2015-10-05_at_1.19.14_PM.png/revision/latest/scale-to-width-down/280?cb=20151005172134',
+          description: 'Dwarf Terrace-9 is a dwarf Earth-like planet with a circumference of 330 yards 808 times smaller than Earth.',
+          price: '0.5 schmeckles'
         }
       ]
     }
--- src/components/PlanetListing.js
+++ src/components/PlanetListing.js
@@ -4,10 +4,13 @@ class PlanetListing extends Component {
   render() {
     return (
       <div className="col-md-4 col-xs-12 planet-listings__planet">
-            <a href="#" className="thumbnail">
-                <img src={this.props.planet.img} alt={this.props.planet.name} />
-                <h3>{this.props.planet.name} <span className="label label-info">New</span></h3>
+            <a href="#">
+                <div className="planet-listings__planet__img">
+                    <img src={this.props.planet.img} alt={this.props.planet.name} />
+                </div>
+                <h3>{this.props.planet.name}</h3>
                 <p>{this.props.planet.description}</p>
+                <h4>{this.props.planet.price}</h4>
                 <button type="button" className="btn btn-default btn-md">
                      View Details
                 </button>
